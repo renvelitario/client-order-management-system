e0478c774b039d376e32e0e9291fa9e2bdda19b6
Ren Velitario <velitarioren@gmail.com>
2026-04-02 23:39:56 +0800

    feat: move error notification to the top of the customer creation form

--- src/pages/customers/CustomerCreate.tsx
+++ src/pages/customers/CustomerCreate.tsx
@@ -32,6 +32,7 @@ const CustomersAdd = () => {
   return (
     <div className="add-customer">
       <h2>Add a Customer</h2>
+      <Notification message={error} type="error" />
       <form onSubmit={handleSubmit}>
         <label>Name:</label>
         <input type="text" name="name" value={formData.name} onChange={handleChange} required /><br />
@@ -40,7 +41,6 @@ const CustomersAdd = () => {
         <label>Contact No:</label>
         <input type="text" name="contact_no" value={formData.contact_no} onChange={handleChange} required /><br />
         <input type="submit" value="Add Customer" />
-        <Notification message={error} type="error" />
       </form>
     </div>
   );
